9f9aa293ff038a256b2571747027597c4c69e615
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-05-04 17:49:54 +0300

    ex1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btn = document.getElementById('btn');
+const stop = document.getElementById('stop');
 
+
+btn.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomHexColor()
+    btn.disabled = true;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+(function changeColor() {
+    setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+      },
+      1000);
+  })();
+
+  stop.addEventListener('click', () => {
+    stop.disabled = true;
+    btn.disabled = false;
+    clearInterval;
+});
