69b486fdc42c02c61ad1f2a987d2f923abe460d7
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-05-04 19:54:07 +0300

    ex 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
-function createPromise(position, delay) {
+let amount = document.getElementsByName("#amount");
+let firstDelay = document.getElementsByName("#delay");
+let delayStep = document.getElementsByName("#step");
+
+for(let i=0; i < amount; i++){
+  const promiseNumber = i + 1;
+  const promiseDelay = firstDelay + i * delayStep;
+  function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    console.log(`Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    console.log(`Rejected promise ${position} in ${delay}ms`);
   }
-}
+};
+};
+
+
+
+
