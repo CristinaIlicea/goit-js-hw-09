d631c16dc861f93b2c2dc15adf275fafb2f1fd25
CristinaIlicea <148708041+CristinaIlicea@users.noreply.github.com>
2024-05-04 16:54:27 +0000

    Deploying to gh-pages from @ CristinaIlicea/goit-js-hw-09@69b486fdc42c02c61ad1f2a987d2f923abe460d7 🚀

---
+++ 03-promises.5b1b0fb3.js
@@ -0,0 +1,2 @@
+!function(){for(var e=document.getElementsByName("#amount"),t=(document.getElementsByName("#delay"),document.getElementsByName("#step"),0);t<e;t++){}}();
+//# sourceMappingURL=03-promises.5b1b0fb3.js.map
---
+++ 03-promises.d9328d99.js
@@ -0,0 +1,2 @@
+let e=document.getElementsByName("#amount");document.getElementsByName("#delay"),document.getElementsByName("#step");for(let t=0;t<e;t++){}
+//# sourceMappingURL=03-promises.d9328d99.js.map
