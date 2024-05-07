ada5b36bba2fdac9154a32e5f0d139d0d7b20546
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-05-07 19:35:12 +0300

    ex 1 corectat

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,24 @@
-const btn = document.getElementById('btn');
-const stop = document.getElementById('stop');
-
-
-btn.addEventListener('click', () => {
-    document.body.style.backgroundColor = getRandomHexColor()
-    btn.disabled = true;
-});
-
-
+//random color
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  };
-
-(function changeColor() {
-    setInterval(() => {
-        document.body.style.background = getRandomHexColor();
-      },
-      1000);
-  })();
-
-  stop.addEventListener('click', () => {
-    stop.disabled = true;
-    btn.disabled = false;
-    function stopEvent() {
-      clearInterval(changeColor);
-}});
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+//button identification and time initialization
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+//comportament start- color background
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor(); //atribuire culoare random la interval de 1ms
+  }, 1000);
+  startBtn.disabled = true; //disable buton start
+  stopBtn.disabled = false; //butonul stop ramane/devine enable
+});
+//stop background color generation
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.disabled = true; //disable buton stop
+  startBtn.disabled = false; //butonul stop ramane/devine enable
+});
