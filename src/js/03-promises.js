36bd1be77af1a815e3b17229585034181612ad8a
CristinaIlicea <ilicea.cristina@yahoo.com>
2024-05-13 14:42:30 +0300

    ex 3 corectat

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,43 @@
-let amount = document.getElementsByName("#amount");
-let firstDelay = document.getElementsByName("#delay");
-let delayStep = document.getElementsByName("#step");
+import { Notify } from 'notiflix';
 
-for(let i=0; i < amount; i++){
-  const promiseNumber = i + 1;
-  const promiseDelay = firstDelay + i * delayStep;
-  function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    console.log(`Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    console.log(`Rejected promise ${position} in ${delay}ms`);
+const form = document.querySelector('form.form');
+const inputDelayEl = document.querySelector('input[name="delay"]');
+const inputStepEl = document.querySelector('input[name="step"]');
+const inputAmountEl = document.querySelector('input[name="amount"]');
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = Number(inputDelayEl.value);
+  let step = Number(inputStepEl.value);
+  let amount = Number(inputAmountEl.value);
+  let position = 0;
+  delay = delay - step;
+  form.reset();
+  for (let i = 0; i < amount; i += 1) {
+    position = i + 1;
+    delay += step;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
-};
-};
-
-
-
-
+}
